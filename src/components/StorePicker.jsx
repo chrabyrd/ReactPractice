73027489ddacfeafb2e587c83525d249e22d2e54
Christopher <chrabyrd@gmail.com>
2017-07-23 18:38:22 -0700

    Capture StorePicker names

--- src/components/StorePicker.jsx
+++ src/components/StorePicker.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 export default class StorePicker extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      storeName: getFunName()
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    console.log('hi');
+  }
+
   render(){
     return(
       <form className="store-selector">
         <h2>Please Enter a Store</h2>
-        <input type="text" placeholder="Store Name" defaultValue={getFunName()}/>
-        <button type="submit">Visit Store</button>
+        <input type="text" placeholder="Store Name" defaultValue={this.state.storeName}/>
+        <button type="submit" onSubmit={this.handleSubmit}>Visit Store</button>
       </form>
     );
   }
